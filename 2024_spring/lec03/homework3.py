3c37361339f14ce131cadee9aa2620834303bcb9
MoonlightStart <49523955+MoonlightStart@users.noreply.github.com>
2024-04-23 11:07:27 +0900

    homework3
    Due to an error in GitHub, all modifications and execution results related to the notebook.ipynb will be uploaded to the king-lms platform in the form of screenshots.

--- 2024_spring/lec03/homework3.py
+++ 2024_spring/lec03/homework3.py
@@ -6,7 +6,16 @@ def cancellation(list, stop_word):
     If one of the elements is equal to the stop_word, then stop the function,
     and return what you have so far.
     '''
-    pass
+    output_list = []
+    n = len(list)
+    for x in range (n):
+        if list[x] != stop_word:
+            output_list.append(list[x])
+        else:
+            break
+
+    return output_list
+
 
 def copy_all_but_skip_word(input_list, skip_word):
     '''
@@ -14,12 +23,25 @@ def copy_all_but_skip_word(input_list, skip_word):
     If one of the elements is equal to the skip_word, then you should skip that element,
     but keep checking all of the other elements.
     '''
-    pass
+    output_list = []
+    n = len(input_list)
+    for x in range(n):
+        if input_list[x] == skip_word:
+            pass
+        else:
+            output_list.append(input_list[x])
+            
+    return output_list
 
 def my_average(input_list):
     '''
     You may assume that `input_list` is a non-empty list, in which every element is a number.  
     Calculate the average value, and return it. 
     '''
-    pass
 
+    average_result = 0
+    n = len(input_list)
+    for x in range(n):
+        average_result += input_list[x]
+
+    return(average_result/n)
