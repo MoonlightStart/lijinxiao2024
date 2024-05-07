2e5ea0eaa024884071a64e77bb1a433c51f5f8d4
MoonlightStart <49523955+MoonlightStart@users.noreply.github.com>
2024-05-07 10:03:22 +0900

    homework4
    Due to an error in GitHub, all modifications and execution results related to the notebook.ipynb will be uploaded to the king-lms platform in the form of screenshots.

--- 2024_spring/lec04/homework4.py
+++ 2024_spring/lec04/homework4.py
@@ -5,8 +5,17 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
+    
+    '''method1
+    return {index: element for index, element in enumerate(input_list)}
+    '''
 
+    '''method2'''
+    return_dict = {}
+    length = len(input_list)  
+    for i in range(length):  
+        return_dict[i] = input_list[i]  
+    return return_dict
 
 
 
